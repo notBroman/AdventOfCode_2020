2ace7b3b73049cf8182298432b9e7e73272a9761
notBroman <romanberger001@gmail.com>
2020-12-06 17:00:04 +0000

    copied answer of part one into new file and changed requirements

--- 06/day6_2.py
+++ 06/day6_2.py
@@ -1,10 +1,10 @@
 # notBroman
 # there is a customs declaration sheet with 26 questions (a-z)
 # you note down the questions groups answer yes to
-# for each group a question counts once at most
+# for each group a question counts once at most and only if everyone picked it
 # groups are split by '\n\n'
 # people in those groups are split by '\n'
-# find the total number of questions that were answered yes
+# find the total number of questions that were answered yes by the whole group
 
 def get_data(fileName):
     with open(fileName, 'r') as f:
