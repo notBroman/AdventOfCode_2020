29e82adec23b2de36bd98301504dcacc06d6d4ca
notBroman <romanberger001@gmail.com>
2020-12-07 18:31:05 +0000

    solution part 1

---
+++ 07/day7_1.py
@@ -0,0 +1,42 @@
+# notBroman
+# there is a list of bags that contain other bags
+# you have a shiny gold bag
+# how many other bags can contain this bag
+
+def get_rules(fileName):
+    with open(fileName, 'r') as inputFile:
+        rules = inputFile.readlines()
+
+    for i in range(len(rules)):
+        rules[i] = rules[i].replace('\n','')
+        rules[i] = rules[i].split(' contain ')
+
+    return rules
+
+def get_bags(listOfRules, newList):
+    length = len(newList)
+
+    for i in range(len(listOfRules)):
+        for j in range(len(newList)):
+            if(newList[j] in listOfRules[i][1] and listOfRules[i][0][:-5] not in newList):
+                print(listOfRules[i][0])
+                newList.append(listOfRules[i][0][:-5])
+
+    if(len(newList) > length):
+        get_bags(listOfRules, newList)
+
+def main():
+
+    name = 'input_day7.txt'
+    test = 'test.txt'
+    listOfBags = ['shiny gold']
+
+    regulations = get_rules(name)
+    get_bags(regulations, listOfBags)
+
+    print(regulations)
+    print(listOfBags)
+    print(len(listOfBags)-1)
+
+main()
+
