50400ccea147634f3dc745f058f550151638cabe
notBroman <romanberger001@gmail.com>
2020-12-09 01:06:50 +0100

    added discription of program

---
+++ 08/day8_1.py
@@ -0,0 +1,12 @@
+# notBroman
+# there is a list instuctions that create a loop
+# run the program until an instuction is executed twice
+# possible instuctions:
+#   nop: no operation
+#   acc: raise a variable by the number following it can be pos or neg
+#   jmp: jump relative to the command to the current line
+
+
+def main():
+    name = 'input_day8.txt'
+
