639bde2740bdd476b5886b42f4f68dd027304ca5
notBroman <romanberger001@gmail.com>
2020-12-09 21:15:21 +0100

    copied solution of part 1

---
+++ 08/day8_2.py
@@ -0,0 +1,50 @@
+# notBroman
+# there is a list instuctions that create a loop
+# run the program until an instuction is executed twice
+# possible instuctions:
+#   nop: no operation
+#   acc: raise a variable by the number following it can be pos or neg
+#   jmp: jump relative to the command to the current line
+
+def get_data(fileName):
+    with open(fileName, 'r') as f:
+        data = f.read().split('\n')
+        del data[len(data)-1]
+
+    for i in range(len(data)):
+        data[i] = data[i].split(' ')
+        data[i][1] = int(data[i][1])
+
+    return data
+
+def execute(myList):
+    i = 0
+    count = 0
+    executedLines = []
+
+    while(i < len(myList)-1):
+        if(i in executedLines):
+            i = len(myList) + 1
+        elif(myList[i][0] == 'acc'):
+            count += myList[i][1]
+            executedLines.append(i)
+            i += 1
+        elif(myList[i][0] == 'jmp'):
+            executedLines.append(i)
+            i += myList[i][1]
+        else:
+            executedLines.append(i)
+            i += 1
+
+    return count
+
+def main():
+    name = 'input_day8.txt'
+    test = 'test.txt'
+
+    instructions = get_data(name)
+    accumulator = execute(instructions)
+    print(instructions)
+    print(accumulator)
+
+main()
