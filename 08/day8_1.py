d6a29ebf0261dd91eac5b6893daf740f757083e1
notBroman <romanberger001@gmail.com>
2020-12-09 20:20:33 +0100

    added get_data()

--- 08/day8_1.py
+++ 08/day8_1.py
@@ -6,7 +6,21 @@
 #   acc: raise a variable by the number following it can be pos or neg
 #   jmp: jump relative to the command to the current line
 
+def get_data(fileName):
+    with open(fileName, 'r') as f:
+        data = f.read().split('\n')
+
+    for i in range(len(data)):
+        data[i] = data[i].split(' ')
+
+    return data
+
 
 def main():
     name = 'input_day8.txt'
+    test = 'test.txt'
 
+    instructions = get_data(test)
+    print(instructions)
+
+main()
