c380cc242a0baea2ad8e48c684f3b4a3da0ef9ce
notBroman <romanberger001@gmail.com>
2020-12-11 01:02:08 +0100

    solution

--- 09/day9_2.py
+++ 09/day9_2.py
@@ -19,6 +19,19 @@ def is_valid(myList, value):
                 return {'Success':True, 'Element': value}
     return {'Success':False, 'Element':value}
 
+def find_set(myList, value):
+    
+    for i in range(len(myList)):
+        total = 0
+        j = i+1
+        while total < value:
+            total = sum(myList[i:j])
+#            print(myList[i:j], total)
+            if(total == value):
+                return myList[i:j]
+            else:
+                j+=1
+
 def main():
     name = 'input_day9.txt'
     test = 'test.txt'
@@ -28,7 +41,12 @@ def main():
     for i in range(preamble, len(data)):
         result = is_valid(data[i-preamble:i],data[i])
         if(result['Success'] == False):
-            print(result)
+            answer = result['Element']
 
+    print(answer)
+
+    answer2 = find_set(data,answer)
+    answer2.sort()
+    print(answer2[0]+answer2[len(answer2)-1])
 
 main()
