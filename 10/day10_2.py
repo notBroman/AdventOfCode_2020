8ab0827c1fea33f17d2c349525872083344dee9b
notBroman <romanberger001@gmail.com>
2020-12-12 19:10:39 +0100

    started the second task of day 10

---
+++ 10/day10_2.py
@@ -0,0 +1,52 @@
+# notBroman
+# count the differences between the elements of the list after it is sorted
+# how many differences are 1, 2 and 3
+# return the profuct of the amuont of differences that are 1 and 3
+
+def get_jolts(fileName):
+    with open(fileName, 'r') as f:
+        data = f.read().split('\n')
+        data.pop(len(data)-1)
+
+
+    for i in range(len(data)):
+        data[i] = int(data[i])
+
+    data.append(0)
+    data.append(max(data)+3)
+
+    return sorted(data)
+
+def get_diffs(myList):
+    diff1 = 0
+    diff2 = 0
+    diff3 = 0
+
+    for i in range(1,len(myList)):
+        diff = myList[i]-myList[i-1]
+        if(diff == 1):
+            diff1 += 1
+        elif(diff == 2):
+            diff2 += 1
+        elif(diff == 3):
+            diff3 += 1
+    return {'diff1':diff1, 'diff2':diff2, 'diff3':diff3}
+
+def get_possible_combinations(myList):
+    myList.reverse()
+    newList = []
+        
+
+
+def main():
+    test = 'test.txt'
+    name = 'input_day10.txt'
+
+    jolts = get_jolts(test)
+    print(jolts)
+    jolt_diff = get_diffs(jolts)
+    print(jolt_diff)
+
+    get_possible_combinations(jolts)
+
+main()
