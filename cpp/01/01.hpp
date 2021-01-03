52e88abeed0b6d8e248ad9d83375cf9951ea1a9f
notBroman <romanberger001@gmail.com>
2021-01-03 21:48:39 +0100

    creation

---
+++ cpp/01/01.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "01.cpp"
+#include <iostream>
+#include <vector>
+#include <regex>
+
+void part1();
+
---
+++ cpp/01/main.cpp
@@ -0,0 +1,15 @@
+//notBroman
+//AOC day 01 but in C++
+
+#include <iostream>
+#include <vector>
+#include <regex>
+
+#include "01.hpp"
+
+int main(){
+    std::cout << "AoC day 01" << "\n" << std::endl;
+
+    part1();
+}
+
